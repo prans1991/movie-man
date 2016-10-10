f2505b339619df8de942e36ad73369cf9d6446bf
G Pranesh Prasad <Pranesh.G1@sc.com>
2016-10-10 10:17:24 +0530

    Add cordova and is in cordova util

---
+++ tests/unit/utils/cordova-test.js
@@ -0,0 +1,10 @@
+import cordova from 'movie-man/utils/cordova';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | cordova');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = cordova();
+  assert.ok(result);
+});
---
+++ tests/unit/utils/is-in-cordova-test.js
@@ -0,0 +1,10 @@
+import isInCordova from 'movie-man/utils/is-in-cordova';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | is in cordova');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = isInCordova();
+  assert.ok(result);
+});
