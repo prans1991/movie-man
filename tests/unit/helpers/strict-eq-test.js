3ebf8640bf1be493edcbd9f17a70f3b91a558872
G Pranesh Prasad <Pranesh.G1@sc.com>
2016-10-10 10:15:14 +0530

    Add strict eq helper

---
+++ tests/unit/helpers/strict-eq-test.js
@@ -0,0 +1,10 @@
+import { strictEq } from 'movie-man/helpers/strict-eq';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | strict eq');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = strictEq([42]);
+  assert.ok(result);
+});
