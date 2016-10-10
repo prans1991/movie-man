655fe40a3e8ab1c4ea158e28fcba8eb2da55e154
G Pranesh Prasad <Pranesh.G1@sc.com>
2016-10-10 09:57:08 +0530

    Play trailer using the videoUrl

--- app/components/movies-by-genre.js
+++ app/components/movies-by-genre.js
@@ -1,4 +1,6 @@
 import Ember from 'ember';
+import isInCordova from 'movie-man/utils/is-in-cordova';
+import cordova from 'movie-man/utils/cordova';
 
 const {observer,isEqual} = Ember;
 
@@ -7,4 +9,18 @@ export default Ember.Component.extend({
      movie: null,
 
      selectedGenre: '',
+
+     playTrailer(videoUrl){
+       if(isInCordova()){
+        window.open(videoUrl,"_blank","location=no","EnableViewPortScale=yes");
+       }else{
+         window.open(videoUrl,"_blank","location=yes","EnableViewPortScale=yes");
+       }
+     },
+
+     actions:{
+       displayTrailer: function(videoUrl){
+         this.playTrailer(videoUrl);
+       }
+     }
 });
