bc4c3bf68a51aab0f37808a31af1a77b7e49e8cc
G Pranesh Prasad <Pranesh.G1@sc.com>
2016-10-20 15:45:58 +0530

    Watch app resume and pause

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
+import configuration from './services/configuration';
 
 let App;
 
@@ -10,9 +11,36 @@ Ember.MODEL_FACTORY_INJECTIONS = true;
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver
+  Resolver,
+  isInBackground: false
 });
+
+// App.isInBackground = false;
+
+App.appDidEnterForeground = function(){
+    Ember.Logger.log("app in foreground"+App.isInBackground);
+    App.isInBackground =false;
+};
+App.appDidEnterBackground=function(){
+  Ember.Logger.log("app in background");
+  App.set('isInBackground',true);
+};
+
+
 
 loadInitializers(App, config.modulePrefix);
 
 export default App;
+
+window.addEventListener('load', function() {
+  document.addEventListener("deviceready", function(){
+  console.log("inside device ready");
+  App.advanceReadiness();
+  }, false);
+  document.addEventListener("resume", function(){
+    App.appDidEnterForeground();
+  },false);
+  document.addEventListener("pause", function(){
+    App.appDidEnterBackground();
+  }, false);
+},false);
