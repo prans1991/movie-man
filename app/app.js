632718ca22a403be702e7f3d7ed029748bd88cd7
G Pranesh Prasad <Pranesh.G1@sc.com>
2016-10-27 12:22:12 +0530

    Change extend to create for creating Ember application object

--- app/app.js
+++ app/app.js
@@ -2,31 +2,27 @@ import Ember from 'ember';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
-import configuration from './services/configuration';
 
+const {inject} = Ember;
 let App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
-App = Ember.Application.extend({
+App = Ember.Application.create({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver,
-  isInBackground: false
-});
-
-// App.isInBackground = false;
-
-App.appDidEnterForeground = function(){
+  isInBackground: false,
+  appController: inject.controller('application'),
+  appDidEnterForeground: function(){
     Ember.Logger.log("app in foreground"+App.isInBackground);
-    App.isInBackground =false;
-};
-App.appDidEnterBackground=function(){
-  Ember.Logger.log("app in background");
-  App.set('isInBackground',true);
-};
-
-
+    this.set('isInBackground',false);
+  },
+  appDidEnterBackground: function(){
+    Ember.Logger.log("app in background");
+    this.set('isInBackground',true);
+  }
+});
 
 loadInitializers(App, config.modulePrefix);
 
@@ -35,7 +31,6 @@ export default App;
 window.addEventListener('load', function() {
   document.addEventListener("deviceready", function(){
   console.log("inside device ready");
-  App.advanceReadiness();
   }, false);
   document.addEventListener("resume", function(){
     App.appDidEnterForeground();
