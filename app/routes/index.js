44b3e204638da38ca93ee65955ee58e5d22a9bbd
G Pranesh Prasad <Pranesh.G1@sc.com>
2016-10-27 12:29:25 +0530

    Import base route to index route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,17 @@
 import Ember from 'ember';
+import App from '../app';
+import BaseRoute from 'movie-man/routes/base';
 
 export default Ember.Route.extend({
 
   redirect: function(){
+    this.set('isInBackground',true);
     this.transitionTo('movies.home');
-  }
+  },
+
+  isInBackground: false,
+
+  appBackgroundEventHandler:function(){
+    Ember.Logger.log("inside background event handler?????????",App.get('isInBackground'));
+  }.observes('isInBackground'),
 });
